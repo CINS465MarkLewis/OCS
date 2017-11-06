4263a7daffadc71b7a1b47a3821504a74a9ec576
Mark Hubrig <mhubrig@mail.csuchico.edu>
2017-11-06 10:55:50 -0800

    added base models and migrations

--- Project/firstapp/models.py
+++ Project/firstapp/models.py
@@ -1,1 +1,16 @@
 from django.db import models
+
+class University(models.Model):
+    uniName = models.CharField(max_length=50) #uniName
+    uniLoca = models.CharField(max_length=50) #uniLocation
+    uniLogo = models.CharField(max_length=1000) #uniLogo
+
+class Org(models.Model):
+    university = models.ForeignKey(University, on_delete=models.CASCADE)
+    #University may need to be removed
+
+    orgName = models.CharField(max_length=50) #orgName
+    orgCateg = models.CharField(max_length=50) #orgCategory
+    orgUni = models.CharField(max_length=100) #orgUniversity
+    orgFoun = models.CharField(max_length=30) #orgFounded
+    orgLogo = models.CharField(max_length=1000) #orgLogo
