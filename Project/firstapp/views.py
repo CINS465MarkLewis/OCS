663f9c7e71c0209cbe51cfae637b28d08341ba7b
Mark Hubrig <tennisslam95@yahoo.com>
2017-11-09 17:54:38 -0800

    Org info and details updated

--- Project/firstapp/views.py
+++ Project/firstapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
+from django.http import Http404
 from .forms import *
 from .models import *
 
@@ -11,6 +12,7 @@ def index(request):
         'all_universities': all_universities
     }
     return render(request, "base.html", context)
+
 def register(request):
     if request.method == 'POST':
         form = registration_form(request.POST)
@@ -23,4 +25,10 @@ def register(request):
     return render(request,"register.html",context)
 
 def detail(request, university_id):
-    return HttpResponse("<h3>Details for University id: " + str(university_id) + "</h2>")
+    #return HttpResponse("<h3>Details for University id: " + str(university_id) + "</h2>")
+    try:
+        #university = University.objects.all()
+        university = University.objects.get(pk=university_id)
+    except University.DoesNotExist:
+        raise Http404("University ID does not exist")
+    return render(request, "org.html", {'university': university})
